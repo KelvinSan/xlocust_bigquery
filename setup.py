6d7f1beea17c055c62a5526951c8d016f98b6075
KelvinSan <57046969+KelvinSan@users.noreply.github.com>
2021-11-12 20:41:38 -0500

    changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xlocust_bigquery',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     url='https://github.com/KelvinSan/xlocust_bigquery',
     license='',
--- xlocust_bigquery/bq_client.py
+++ xlocust_bigquery/bq_client.py
@@ -14,27 +14,22 @@ class BQClientUser(User):
 
     def __init__(self, environment):
         super().__init__(environment)
+        self.account = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
-    def client(self):
-
-        account = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
-
-        if account is None:
+        if self.account is None:
             raise Exception("Please ensure that service account enviornment varible is set before using user")
 
-        credentials = service_account.Credentials.from_service_account_file(account, scopes=[
+        self.credentials = service_account.Credentials.from_service_account_file(self.account, scopes=[
             "https://www.googleapis.com/auth/cloud-platform"])
 
-        bqclient = bigquery.Client(credentials=credentials, project=credentials.project_id)
-
-        return bqclient
+        self.bqclient = bigquery.Client(credentials=self.credentials, project=self.credentials.project_id)
     
-    def query(self,client,query):
+    def query(self,query):
 
         start_time = time.time()
 
         try:
-            bq_query = client.query(query)
+            bq_query = self.bqclient.query(query)
             results = bq_query.result()
             data = results
             self.environment.events.request.fire(
@@ -46,6 +41,7 @@ class BQClientUser(User):
             exception=None,
     )
             return data.to_dataframe()
+
         except Exception as e:
             print(e)
             self.environment.events.request.fire(
@@ -57,16 +53,44 @@ class BQClientUser(User):
             exception=e,
     )
 
-    # def dataload_csv_gsbucket(self,environment, uri, table_id,response_length,start_time,err, _msg):
-        
-    #     self.environment.events.request.fire(
-    #     request_type="GCPBQ",
-    #     name=uri+"_"+table_id,
-    #     response_time=(time.monotonic() - start_time) * 1000,
-    #     response_length=response_length,
-    #     context={},
-    #     exception=err,
-    # )
+    def dataload_csv_gsbucket(self,uri, table_id):     
+        start_time = time.time()
+        try:
+            job_config = bigquery.LoadJobConfig(
+            autodetect=False,
+            skip_leading_rows=1,
+            source_format=bigquery.SourceFormat.CSV
+            )
+            job_config.schema_update_options = [
+                bigquery.SchemaUpdateOption.ALLOW_FIELD_RELAXATION
+            ]
+            load_job = self.bqclient.load_table_from_uri(uri, table_id, job_config=job_config)
+
+            load_job.result()
+
+            destination_table = self.bqclient.get_table(table_id)
+            print("Loaded {} rows.".format(destination_table.num_rows))
+            self.environment.events.request.fire(
+            request_type="GCPBQ",
+            name=uri+"_"+table_id,
+            response_time=int((time.time() - start_time) * 1000),
+            response_length=int((time.time() - start_time) * 1000),
+            context={},
+            exception=None
+            )
+
+            return destination_table.num_rows
+            
+        except Exception as e:
+            print(e)
+            self.environment.events.request.fire(
+            request_type="GCPBQ",
+            name=uri+"_"+table_id,
+            response_time=int((time.time() - start_time) * 1000),
+            response_length=int((time.time() - start_time) * 1000),
+            context={},
+            exception=e,
+    )
 
 
     
